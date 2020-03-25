ecf83b208f2b3c37127c1e90f047c9e97e3d5840
Kenn3Th <Kenneth.Eikrehagen@gmail.com>
2020-03-25 18:01:24 +0100

    Edit program

--- test.py
+++ test.py
@@ -5,4 +5,13 @@ y = 3
 def pytagoras(a, b):
     return sqrt(a**2+b**2)
 
-z = pytagoras(x,y)
+def test_pytagoras():
+    tolerance = 1e-10
+
+    computed = pytagoras(3,4)
+    expected = 5
+    success = abs(expected - computed[0]) < tol
+    msg = "Something went wrong!"
+    assert success, msg
+
+test_pytagoras()
